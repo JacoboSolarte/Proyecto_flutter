21c7a06bef01a0803c3f6a2fd0d142424e7c0c99
JeronimoUnigarro <jeronimo.unigarro@campusuccl.edu.co>
2025-10-29 01:27:46 -0500

    correcciones

--- supabase/functions/extract_equipment_from_image/deno-stubs.d.ts
+++
@@ -1,7 +0,0 @@
-// Minimal Deno types for editor/tsserver without Deno lib support.
-declare const Deno: {
-  serve: (handler: (req: Request) => Response | Promise<Response>) => void;
-  env: {
-    get(name: string): string | undefined;
-  };
-};
